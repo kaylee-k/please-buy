c88e482e807356ddbaa80f27ef38fdc3060ac7d9
kaylee-k <kaylee.kihm@gmail.com>
2024-08-12 22:05:27 -0600

    dynamically display items with updated layout and styling

--- src/main.js
+++ src/main.js
@@ -5,6 +5,21 @@ function loadItems() {
     .then((json) => json.items);
 }
 
+// Update the list with the given items
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map((item) => createHTMLString(item)).join('');
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+  return ` 
+    <li class="item">
+        <img src="${item.image}" alt="${item.brand}" class="item__thumbnail" />
+         <span class="item__description">${item.devices}, ${item.capacity}</span>
+    </li>
+        `;
+}
 loadItems()
   .then((items) => {
     displayItems(items);
